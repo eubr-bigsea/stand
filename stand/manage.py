ab3b78bd4f6a926cb0088645ba1309cac2f55dbe
Walter dos Santos Filho <waltersf@gmail.com>
2017-02-22 13:43:53 -0300

    Fixing problems with migration

---
+++ stand/manage.py
@@ -0,0 +1,14 @@
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+from stand.app import app
+from stand.models import db
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == "__main__":
+    manager.run()
