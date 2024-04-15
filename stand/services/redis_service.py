565d2f80daeac4416feb801a07efae11a9218bb8
Walter dos Santos Filho <waltersf@gmail.com>
2024-04-15 09:23:04 -0300

    Implementation

--- stand/services/redis_service.py
+++ stand/services/redis_service.py
@@ -16,14 +16,14 @@ class MockRedisWrapper(MockRedis):
         return cls()
 
 
-def connect_redis_store(url, testing=False, decode_responses=True):
+def connect_redis_store(url, testing=False, decode_responses=True, db=0):
     """ Connect to redis or FakeRedis if testing app """
 
     def _redis_from_url(redis_url):
         parsed = urllib.parse.urlparse(redis_url)
         parsed_qs = urllib.parse.parse_qs(parsed.query)
         return FlaskRedis(host=parsed.hostname, port=parsed.port,
-                          db=int(parsed_qs.get('db', 0)),
+                          db=int(parsed_qs.get('db', db)),
                           decode_responses=decode_responses)
     if testing or (current_app and current_app.testing):
         redis_store = FlaskRedis.from_custom_provider(MockRedisWrapper)
