0330de1939e969972d1ce09f0859516e42b92aa5
samuellipovetsky <samuellpvt@gmail.com>
2024-05-01 16:34:44 -0300

    todo: fix  step_has_associated_active_job

--- stand/scheduler/trigger_scheduled_jobs.py
+++ stand/scheduler/trigger_scheduled_jobs.py
@@ -2,110 +2,120 @@ import asyncio
 import os
 import typing
 from datetime import date, datetime, timedelta
-from typing import List 
+from typing import List
 import requests
 import yaml
 from croniter import croniter
 from sqlalchemy import func, select
-from sqlalchemy.ext.asyncio import (
-    AsyncEngine, AsyncSession, create_async_engine)
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import and_
 import json
-from stand.models import (Job, PipelineRun, PipelineStepRun, StatusExecution,Pipeline,PipelineStep,TriggerType)
+from stand.models import (
+    Job,
+    PipelineRun,
+    PipelineStepRun,
+    StatusExecution,
+    Pipeline,
+    PipelineStep,
+    TriggerType,
+)
 from stand.scheduler.utils import *
 
 
-def is_next_step_in_order(step:PipelineStep,pipeline_run:PipelineRun)-> bool:
+def is_next_step_in_order(step: PipelineStep, pipeline_run: PipelineRun) -> bool:
     "returns if step is the next step in a pipeline run execution"
     pass
+
 
 def step_has_associated_active_job():
     "name"
     pass
 
-def time_match(scheduling,current_time:datetime)->bool:
-    
+
+def time_match(scheduling, current_time: datetime) -> bool:
     """
     returns if datetime matchs with scheduling json format from a pipeline_step
     to the minute
     """
+
     parsed_scheduling = json.loads(scheduling)
-    
     start_time = get_step_start_time(scheduling)
-    
-    #prevents matches before start_time
-    if(start_time > current_time):
-            return False
-    
-    if(parsed_scheduling["stepSchedule"]["frequency"]=="once"):
-        if (start_time==current_time):
+
+    # prevents matches before start_time
+    if start_time > current_time:
+
+        return False
+
+    if parsed_scheduling["stepSchedule"]["frequency"] == "once":
+
+        if start_time == current_time:
             return True
         else:
             return False
-        
-    if(parsed_scheduling["stepSchedule"]["frequency"]=="daily"):
-   
-        delta = abs(current_time- start_time)
+
+    if parsed_scheduling["stepSchedule"]["frequency"] == "daily":
+
+        delta = abs(current_time - start_time)
         difference_in_minutes = delta.total_seconds() / 60
-        
-        frequency_in_days =parsed_scheduling["stepSchedule"]["intervalDays"]
-        #diference in days  is divisible by frenquency in days , so match 
-        return difference_in_minutes % (int(frequency_in_days)*1440) == 0
-    
-    if(parsed_scheduling["stepSchedule"]["frequency"]=="monthly"):
+
+        frequency_in_days = parsed_scheduling["stepSchedule"]["intervalDays"]
+        # diference in days  is divisible by frenquency in days , so match
+        return difference_in_minutes % (int(frequency_in_days) * 1440) == 0
+
+    if parsed_scheduling["stepSchedule"]["frequency"] == "monthly":
+
         current_month = str(current_time.month)
-        current_day  = str(current_time.day)
+        current_day = str(current_time.day)
         current_hour = current_time.hour
-        currrent_minute =current_time.minute
-        scheduled_months =  parsed_scheduling["stepSchedule"]["months"]
-        scheduled_days  =  parsed_scheduling["stepSchedule"]["days"]
+        currrent_minute = current_time.minute
+        scheduled_months = parsed_scheduling["stepSchedule"]["months"]
+        scheduled_days = parsed_scheduling["stepSchedule"]["days"]
         scheduled_hour = start_time.hour
         scheduled_minute = start_time.minute
-       
-        if(scheduled_hour==current_hour and scheduled_minute==currrent_minute):
-            if(current_month in scheduled_months and current_day in scheduled_days):
+
+        if scheduled_hour == current_hour and scheduled_minute == currrent_minute:
+            if current_month in scheduled_months and current_day in scheduled_days:
                 return True
         return False
-        
-def get_step_is_immediate(scheduling)->bool:
- 
-    parsed_scheduling = json.loads(scheduling)
-    return parsed_scheduling["stepSchedule"]["executeImmediately"] 
 
-def get_step_start_time(scheduling)->datetime:
-    
+
+def get_step_is_immediate(scheduling) -> bool:
+
     parsed_scheduling = json.loads(scheduling)
-    
+    return parsed_scheduling["stepSchedule"]["executeImmediately"]
+
+
+def get_step_start_time(scheduling) -> datetime:
+
+    parsed_scheduling = json.loads(scheduling)
     start_datetime_str = parsed_scheduling["stepSchedule"]["startDateTime"]
-
     start_datetime_obj = datetime.strptime(start_datetime_str, "%Y-%m-%dT%H:%M:%S")
-    
     return start_datetime_obj
 
 
-def trigger_scheduled_pipeline_steps(pipeline_run:PipelineRun, time:datetime):
-    
-    steps= get_pipeline_steps(pipeline_run)
-    
+def trigger_scheduled_pipeline_steps(pipeline_run: PipelineRun, time: datetime):
+
+    steps = get_pipeline_steps(pipeline_run)
+
     for step in steps:
-        #scheduled job
-        if(not get_step_is_immediate(step.scheduling)):
-            if(time_match(step.scheduling,time)): 
-                if(is_next_step_in_order(step,pipeline_run)):
-                    #both time and order match
+
+        # time scheduled job
+        if not get_step_is_immediate(step.scheduling):
+            if is_next_step_in_order(step, pipeline_run):
+                if time_match(step.scheduling, time):
+
+                    # both time and order match
                     create_step_run(step)
                     return step
                 else:
-                    #only time match, execution out of order
-                    update_pipeline_run_status(pipeline_run,StatusExecution.PENDING)
-                    return step
-        
-        if(get_step_is_immediate(step.scheduling)): # steps that occur imediatelly after the last one
-           if(is_next_step_in_order(step,pipeline_run)):
+                    # only time match, execution out of order
+                    update_pipeline_run_status(pipeline_run, StatusExecution.PENDING)
+
+        # steps that occur imediatelly after the last one
+        if get_step_is_immediate(step.scheduling):
+
+            if is_next_step_in_order(step, pipeline_run):
+
                 create_step_run(step)
                 return step
-                
-            
-    
-    
--- tests/test_status_propagation.py
+++ tests/test_status_propagation.py
@@ -1,109 +1,116 @@
-
 from collections import KeysView
 from datetime import date, datetime, timedelta
 
 import pytest
-from mock import ANY, patch,MagicMock
+from mock import ANY, patch, MagicMock
 from mock.mock import AsyncMock
 
-from stand.models import PipelineRun, StatusExecution ,PipelineStep
+from stand.models import PipelineRun, StatusExecution, PipelineStep
 from stand.scheduler.utils import *
 from stand.scheduler.status_control import *
 
+
 @pytest.fixture
 def mocked_functions():
-    with patch("stand.scheduler.status_control.get_latest_pipeline_step_run") as mocked_get_latest_pipeline_step_run, \
-         patch("stand.scheduler.status_control.get_latest_job_from_pipeline_step_run") as mocked_get_latest_job_from_pipeline_step_run, \
-         patch("stand.scheduler.status_control.update_pipeline_run_status") as mocked_update_pipeline_run_status, \
-         patch("stand.scheduler.status_control.update_pipeline_step_run_status") as mocked_update_pipeline_step_run_status:
-        
-        yield (mocked_get_latest_pipeline_step_run,
-               mocked_get_latest_job_from_pipeline_step_run,
-               mocked_update_pipeline_run_status,
-               mocked_update_pipeline_step_run_status)
+    with patch(
+        "stand.scheduler.status_control.get_latest_pipeline_step_run"
+    ) as mocked_get_latest_pipeline_step_run, patch(
+        "stand.scheduler.status_control.get_latest_job_from_pipeline_step_run"
+    ) as mocked_get_latest_job_from_pipeline_step_run, patch(
+        "stand.scheduler.status_control.update_pipeline_run_status"
+    ) as mocked_update_pipeline_run_status, patch(
+        "stand.scheduler.status_control.update_pipeline_step_run_status"
+    ) as mocked_update_pipeline_step_run_status:
+
+        yield (
+            mocked_get_latest_pipeline_step_run,
+            mocked_get_latest_job_from_pipeline_step_run,
+            mocked_update_pipeline_run_status,
+            mocked_update_pipeline_step_run_status,
+        )
+
 
 def test_completed_job_status_propagation(mocked_functions):
     """
     tests if a "completed" job  has its status
     correctly propagated to its associated step_run and pipeline_run
     """
-    mocked_get_latest_pipeline_step_run, \
-    mocked_get_latest_job_from_pipeline_step_run, \
-    mocked_update_pipeline_run_status, \
-    mocked_update_pipeline_step_run_status = mocked_functions
-    latest_job =Job(id=1, status = StatusExecution.COMPLETED)
-    
-    step_run =PipelineStepRun(id="1", 
-                           status = StatusExecution.RUNNING)
-    
-    pipeline_run = PipelineRun(id= "1",
-                               status= StatusExecution.RUNNING)
-    
+    (
+        mocked_get_latest_pipeline_step_run,
+        mocked_get_latest_job_from_pipeline_step_run,
+        mocked_update_pipeline_run_status,
+        mocked_update_pipeline_step_run_status,
+    ) = mocked_functions
+    latest_job = Job(id=1, status=StatusExecution.COMPLETED)
+
+    step_run = PipelineStepRun(id="1", status=StatusExecution.RUNNING)
+
+    pipeline_run = PipelineRun(id="1", status=StatusExecution.RUNNING)
+
     mocked_get_latest_pipeline_step_run.return_value = step_run
     mocked_get_latest_job_from_pipeline_step_run.return_value = latest_job
-    
+
     propagate_job_status(pipeline_run)
-    
-    mocked_update_pipeline_run_status.assert_called_with(pipeline_run,
-                                                              StatusExecution.WAITING)
-    mocked_update_pipeline_step_run_status(pipeline_run,
-                                                         StatusExecution.COMPLETED)
+
+    mocked_update_pipeline_run_status.assert_called_with(
+        pipeline_run, StatusExecution.WAITING
+    )
+    mocked_update_pipeline_step_run_status(pipeline_run, StatusExecution.COMPLETED)
+
 
 def test_completed_job_status_propagation(mocked_functions):
     """
     tests if a job  thast started to run has its status
     correctly propagated to its associated "waiting"step_run and pipeline_run
     """
-    mocked_get_latest_pipeline_step_run, \
-    mocked_get_latest_job_from_pipeline_step_run, \
-    mocked_update_pipeline_run_status, \
-    mocked_update_pipeline_step_run_status = mocked_functions
-    
-    latest_job =Job(id=1, status = StatusExecution.RUNNING)
-    
-    step_run =PipelineStepRun(id="1", 
-                           status = StatusExecution.RUNNING)
-    
-    pipeline_run = PipelineRun(id= "1",
-                               status= StatusExecution.WAITING)
-    
+    (
+        mocked_get_latest_pipeline_step_run,
+        mocked_get_latest_job_from_pipeline_step_run,
+        mocked_update_pipeline_run_status,
+        mocked_update_pipeline_step_run_status,
+    ) = mocked_functions
+
+    latest_job = Job(id=1, status=StatusExecution.RUNNING)
+
+    step_run = PipelineStepRun(id="1", status=StatusExecution.RUNNING)
+
+    pipeline_run = PipelineRun(id="1", status=StatusExecution.WAITING)
+
     mocked_get_latest_pipeline_step_run.return_value = step_run
     mocked_get_latest_job_from_pipeline_step_run.return_value = latest_job
-    
+
     propagate_job_status(pipeline_run)
-    
-    mocked_update_pipeline_run_status.assert_called_with(pipeline_run,
-                                                              StatusExecution.RUNNING)
-    
+
+    mocked_update_pipeline_run_status.assert_called_with(
+        pipeline_run, StatusExecution.RUNNING
+    )
+
+
 def test_error_job_status_propagation(mocked_functions):
     """
     tests if a error status job has its status
     correctly propagated to its associated "waiting"step_run and pipeline_run
     """
-    
-    mocked_get_latest_pipeline_step_run, \
-    mocked_get_latest_job_from_pipeline_step_run, \
-    mocked_update_pipeline_run_status, \
-    mocked_update_pipeline_step_run_status = mocked_functions
-    
-    latest_job =Job(id=1, status = StatusExecution.ERROR)
-    
-    step_run =PipelineStepRun(id="1", 
-                           status = StatusExecution.RUNNING)
-    
-    pipeline_run = PipelineRun(id= "1",
-                               status= StatusExecution.WAITING)
-    
+
+    (
+        mocked_get_latest_pipeline_step_run,
+        mocked_get_latest_job_from_pipeline_step_run,
+        mocked_update_pipeline_run_status,
+        mocked_update_pipeline_step_run_status,
+    ) = mocked_functions
+
+    latest_job = Job(id=1, status=StatusExecution.ERROR)
+
+    step_run = PipelineStepRun(id="1", status=StatusExecution.RUNNING)
+
+    pipeline_run = PipelineRun(id="1", status=StatusExecution.WAITING)
+
     mocked_get_latest_pipeline_step_run.return_value = step_run
     mocked_get_latest_job_from_pipeline_step_run.return_value = latest_job
-    
+
     propagate_job_status(pipeline_run)
-    
-    mocked_update_pipeline_run_status.assert_called_with(pipeline_run,
-                                                        StatusExecution.ERROR)
-    mocked_update_pipeline_step_run_status(pipeline_run,
-                                            StatusExecution.ERROR)
-  
-    
-   
-  
+
+    mocked_update_pipeline_run_status.assert_called_with(
+        pipeline_run, StatusExecution.ERROR
+    )
+    mocked_update_pipeline_step_run_status(pipeline_run, StatusExecution.ERROR)
