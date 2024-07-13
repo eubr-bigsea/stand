0e518052ac45e733847cb0156615e7a4f29c68b7
Walter dos Santos Filho <waltersf@gmail.com>
2024-07-13 09:14:34 -0300

    Fix problem with recursin during serialization

--- stand/schema.py
+++ stand/schema.py
@@ -987,6 +987,10 @@ class PipelineStepRunCreateRequestSchema(BaseSchema):
                            validate=[OneOf(StatusExecution.values())])
     final_status = fields.String(required=False, allow_none=True,
                                  validate=[OneOf(StatusExecution.values())])
+    jobs = fields.Nested(
+        'stand.schema.JobCreateRequestSchema',
+        allow_none=True,
+        many=True)
     pipeline_run = fields.Nested(
         'stand.schema.PipelineRunCreateRequestSchema',
         required=True)
@@ -1035,9 +1039,6 @@ class PipelineStepRunListResponseSchema(BaseSchema):
         'stand.schema.JobListResponseSchema',
         allow_none=True,
         many=True)
-    pipeline_run = fields.Nested(
-        'stand.schema.PipelineRunListResponseSchema',
-        required=True)
     logs = fields.Nested(
         'stand.schema.PipelineStepRunLogListResponseSchema',
         required=True,
@@ -1083,9 +1084,6 @@ class PipelineStepRunItemResponseSchema(BaseSchema):
         'stand.schema.JobItemResponseSchema',
         allow_none=True,
         many=True)
-    pipeline_run = fields.Nested(
-        'stand.schema.PipelineRunItemResponseSchema',
-        required=True)
     logs = fields.Nested(
         'stand.schema.PipelineStepRunLogItemResponseSchema',
         required=True,
@@ -1131,9 +1129,6 @@ class PipelineStepRunCreateRequestSchema(BaseSchema):
         'stand.schema.JobCreateRequestSchema',
         allow_none=True,
         many=True)
-    pipeline_run = fields.Nested(
-        'stand.schema.PipelineRunCreateRequestSchema',
-        required=True)
     logs = fields.Nested(
         'stand.schema.PipelineStepRunLogCreateRequestSchema',
         required=True,
