d51bd6b9b4ab600564d319439abd409db790a164
Walter dos Santos Filho <waltersf@gmail.com>
2026-07-28 18:14:23 -0300

    Fix id in pydantic

--- stand/schema.py
+++ stand/schema.py
@@ -1575,6 +1575,7 @@ class GlobalVariableListResponseSchema(BaseSchema):
 class GlobalVariableItemResponseSchema(BaseSchema):
     """JSON serialization schema for serializing a single object"""
 
+    id = fields.Integer(required=True)
     name = fields.String(required=True)
     enabled = fields.Boolean(required=True)
     description = fields.String(required=False, allow_none=True)
@@ -1594,7 +1595,6 @@ class GlobalVariableItemResponseSchema(BaseSchema):
 class GlobalVariableCreateRequestSchema(BaseSchema):
     """JSON serialization schema for creating an instance"""
 
-    id = fields.Integer(required=True)
     name = fields.String(required=True)
     enabled = fields.Boolean(required=True)
     description = fields.String(required=False, allow_none=True)
